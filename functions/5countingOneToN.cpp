62988f7bad352ab87614476365ec0dcb5619def5
DHARM <opdsbanasya@gmail.com>
2024-08-09 08:57:32 +0530

    countingOneToN

---
+++ functions/5countingOneToN.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+void counting(int n){
+    for(int i=1; i<=n; i++){
+        cout<<i<<" ";
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter value of N"<<endl;
+    cin>>n;
+
+    counting(n);
+    return 0;
+}
