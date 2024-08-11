0bfe9d3d8ad75e86a7f7506073abd8a049433906
DHARM <opdsbanasya@gmail.com>
2024-08-11 19:02:10 +0530

    set kth bit

---
+++ functions/15setKthBit.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int setKthBit(int n, int k){
+    int mask = 1<<k;
+    int ans = mask | n;
+    return ans;
+}
+
+int main(){
+    int n, k;
+    cout<<"Enter number and kth num"<<endl;
+    cin>>n>>k;
+    int ans = setKthBit(n, k);
+    cout<<ans;
+    return 0;
+}
