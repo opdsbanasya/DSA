25ac53534610597eb84c611698ee2610b746d18e
DHARM <opdsbanasya@gmail.com>
2024-08-12 19:08:53 +0530

    Number palindrome or not

--- functions/17numPalindromeOrNot.cpp
+++ functions/17numPalindromeOrNot.cpp
@@ -3,13 +3,18 @@
 using namespace std;
 
 bool checkPalindrome(int x){
+    if(n<=INT64_MIN){
+        return 0;
+    }
     int ans = 0;
     int num = x;
     while(x>0){
+        if(ans>INT64_MAX/10){
+            return 0;
+        }
         ans = ans*10 + x%10;
         x /= 10;
     }
-    if(ans<=INT32_MIN || ans>INT32_MAX) return false; 
     if(ans == num) return true;
     return false;
 }
