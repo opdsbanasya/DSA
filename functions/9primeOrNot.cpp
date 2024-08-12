e688d88cbcf43fb501141f2afafd0a2aae82e793
DHARM <opdsbanasya@gmail.com>
2024-08-12 17:11:04 +0530

    prime or not

--- functions/9primeOrNot.cpp
+++ functions/9primeOrNot.cpp
@@ -5,23 +5,24 @@ using namespace std;
 bool primeOrNot(int num){
     for(int i=2; i<num; i++){
         if(num%i == 0){
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 int main(){
     int num;
     cout<<"Enter the Num"<<endl;
     cin>>num;
-
-    if(num == 1){
-        cout<<"The number is nor Prime neither Composite"<< endl;
-    } else if(primeOrNot(num)){
-        cout<<"The number is Composite"<< endl;
-    } else{
+    if(num == 1 || num == 0){
+        cout<< "It is a not prime or not composite";
+        return 0;
+    }
+    if(primeOrNot(num)){
         cout<<"The number is Prime"<<endl;
+    } else{
+        cout<<"The number is Composite"<< endl;
     }
     return 0;
 }
