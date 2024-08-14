1e6acee2359af09958a20d83d84df966164ef582
DHARM <opdsbanasya@gmail.com>
2024-08-14 20:44:14 +0530

    basic

--- Array/1basicsOfArray.cpp
+++ Array/1basicsOfArray.cpp
@@ -8,6 +8,17 @@ int main() {
     cout<<arr[1]<<" ";
     cout<<arr[2]<<" ";
     cout<<arr[3]<<" ";
+    cout<<endl<<endl;
+
+    int arr[50];
+
+    cout<<"Enter the<< element"<<endl;
+    for(int i=0; i<5; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<5; i++){
+        cout<<2*arr[i]<<" ";
+    }
 
     return 0;
 }
