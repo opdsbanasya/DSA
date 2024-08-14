1a84cd99a5678f37890c073c3f9a60623add6644
DHARM <opdsbanasya@gmail.com>
2024-08-14 20:20:45 +0530

    basics of array

--- Array/2printDouble.cpp
+++ Array/2printDouble.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(){
-    int arr[5];
+    int arr[50];
 
+    cout<<"Enter the<< element"<<endl;
     for(int i=0; i<5; i++){
         cin>>arr[i];
     }
--- Array/README.md
+++ Array/README.md
@@ -1,3 +1,9 @@
 Array
 why
 memset function
+
+linear search
+count 0's and 1's
+max and min element
+extreme print
+reverse array
