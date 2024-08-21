849f321b199d4ac0dd187772c86f892e9d428b10
DHARM <opdsbanasya@gmail.com>
2024-08-22 00:46:50 +0530

    find missing number by sorting+swapping  method

--- Vector/12findMissing.cpp
+++ Vector/12findMissing.cpp
@@ -2,9 +2,10 @@
 #include<vector>
 
 using namespace std;
+
 void findDuplicate(vector<int> num){
     // visited method
-    for(int i=0; i<num.size(); i++){
+    /* for(int i=0; i<num.size(); i++){
         int index = abs(num[i]);
         if(num[index-1]>0){
             num[index-1] *= -1;
@@ -14,11 +15,28 @@ void findDuplicate(vector<int> num){
         if(num[i]>0){
             cout<<i+1<<" ";
         }
+    } */
+
+    // swapping + sorting method
+    int i = 0;
+    while(i<num.size()){
+        int index = num[i] - 1;
+        if(num[i] != num[index]){
+            swap(num[i], num[index]);
+        } else {
+            ++i;
+        }
+    }
+    for(int i=0; i<num.size(); i++){
+        if(num[i] != i+1){
+            cout<<i+1<<" ";
+        }
     }
 
 }
 int main(){
-    vector<int> num{1,3,5,3,4};
+
+    vector<int> num{1,3,4,3,5};
     findDuplicate(num);
     // cout<<missing<<endl;
 
