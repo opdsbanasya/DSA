ffc6be4f687a8e88984bf32f182692603fa287d4
DHARM <opdsbanasya@gmail.com>
2024-08-27 19:23:04 +0530

    sum of tho nums there digit stored in an array update

--- Vector/17addNumsInArray.cpp
+++ Vector/17addNumsInArray.cpp
@@ -4,23 +4,23 @@
 #include <algorithm>
 
 using namespace std;
-string arraySum(vector<int> arr1, vector<int> arr2){
+vector<int> arraySum(vector<int> arr1, vector<int> arr2){
     int carry = 0;
-    string ans;
+    vector<int> ans;
     int i = arr1.size()-1;
     int j = arr2.size()-1;
 
     while(i>=0 || j>=0){
         int x=(i>=0 ? arr1[i] : 0) + (j>=0 ? arr2[j] : 0) + carry;
-        ans.push_back(x%10+'0');
+        ans.push_back(x%10);
         carry = x/10;
         i--, j--;
     }
     
     if(carry){
-        ans.push_back(carry+'0');
+        ans.push_back(carry);
     }
-    while(ans[ans.size()-1] == '0'){
+    while(ans[ans.size()-1] == 0){
         ans.pop_back();
     }
     reverse(ans.begin(), ans.end());
@@ -30,7 +30,7 @@ int main(){
     vector<int> arr1{9,5,4,9};
     vector<int> arr2{2,1,4};
 
-    string ans = arraySum(arr1, arr2);
+    vector<int> ans = arraySum(arr1, arr2);
 
     cout << "All  Elements" << endl;
     for (int i = 0; i < ans.size(); i++)
