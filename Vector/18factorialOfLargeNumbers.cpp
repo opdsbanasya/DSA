1fdaa096106ad969a1967ca696eca8314a302e25
DHARM <opdsbanasya@gmail.com>
2024-08-27 19:17:22 +0530

    factorial of large numbers

---
+++ Vector/18factorialOfLargeNumbers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> largeFatorial(int n){
+    vector<int> ans;
+    ans.push_back(1);
+    int carry = 0;
+
+    for(int i=2; i<=n; i++){
+        for(int j=0; j<ans.size(); j++){
+            int x=ans[j]*i + carry;
+            ans[j] = x%10;
+            carry = x/10;
+        }
+        while(carry){
+            ans.push_back(carry%10);
+            carry /= 10;
+        }
+    }
+    
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+
+    vector<int> ans = largeFatorial(n);
+
+    cout << "Factorial of "<<n<<" is : " << endl;
+    for (int i = 0; i < ans.size(); i++){
+        cout<< ans[i];
+    }
+
+    return 0;
+}
