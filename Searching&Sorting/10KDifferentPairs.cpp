4487ff32a69cd62fa940db25e5dd954667b08b5d
DHARM <opdsbanasya@gmail.com>
2024-10-12 00:43:16 +0530

    using binary

--- Searching&Sorting/10KDifferentPairs.cpp
+++ Searching&Sorting/10KDifferentPairs.cpp
@@ -27,11 +27,39 @@ int findPairs(int nums[], int size, int k) {
     return ans.size();
 }
 
+int binarySearch(int nums[],int start, int size, int x){
+    int end = size - 1;
+    while(start <= end){
+        int mid = start + (end-start)/2;
+        if(nums[mid] == x) return mid;
+        else if(nums[mid] > x) end = mid - 1;
+        else start = mid + 1;
+    }
+    return -1;
+}
+int findPairsBS(int nums[], int size, int k) {
+    set<pair<int, int>> ans;
+    sort(nums, nums + size);
+    int i = 0;
+
+    while(i<size){
+        if(binarySearch(nums, i+1, size, nums[i] + k) != -1){
+            ans.insert({nums[i], nums[i]+k});
+            cout<<"nums[i] = "<<nums[i]<<", nums[i] + k = "<<nums[i]+k<<endl;
+        }
+        i++;
+    }
+
+    return ans.size();
+}
+
 int main() {
-    int nums[] = {1, 1, 1, 1, 1};
-    int k = 0;
+    int nums[] = {3,1,4,1,5};
+    int k = 2;
     int size = sizeof(nums) / sizeof(nums[0]);
-    int numberOfPairs = findPairs(nums, size, k);
-    cout << numberOfPairs << endl;
+    // int numberOfPairs = findPairs(nums, size, k);
+    int numberOfPairsBS = findPairsBS(nums, size, k);
+    // cout << numberOfPairs << endl;
+    cout << numberOfPairsBS << endl;
     return 0;
 }
