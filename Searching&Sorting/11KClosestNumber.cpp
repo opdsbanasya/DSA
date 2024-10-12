33266a03897b31cae568ccf5b6c81b45d21b17e1
DHARM <opdsbanasya@gmail.com>
2024-10-12 19:09:40 +0530

    using binary search

--- Searching&Sorting/11KClosestNumber.cpp
+++ Searching&Sorting/11KClosestNumber.cpp
@@ -31,22 +31,65 @@ vector<int> kClosest(vector<int> nums, int k, int x) {
         i++;
     }
 
-    for(int i = l; i<h+1; i++){
-        cout<<nums[i]<<" "<<endl;
-        ans.push_back(nums[i]);
+    for(int j = l; j<h+1; j++){
+        cout<<nums[j]<<" "<<endl;
+        ans.push_back(nums[j]);
     }
 
     printlg(ans);
     return ans;
 }
 
+int firstOccurence(vector<int> nums, int target){
+    int start = 0;
+    int end = nums.size()-1;
+    int mid = start + (end - start)/2;
+
+    int ans = end;
+    while(start<=end){
+        if(nums[mid] >= target){
+            ans = mid;
+            end = mid - 1;
+        } 
+        else start = mid + 1;
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+
+vector<int> kClosestBS(vector<int> nums, int k, int x) {
+    sort(nums.begin(), nums.end());
+    int h = firstOccurence(nums, x);
+    int l = h-1;
+
+    while(k--){
+        cout<<"k = "<<k<<" l = "<<l<<" h = "<<h<<endl;
+        cout<<"nums[l] = "<<nums[l]<<" nums[h] = "<<nums[h]<<endl<<x-nums[l]<<" > "<<nums[h]-x<<endl<<endl;
+        if(l<0) h++;
+        else if(h>=nums.size()) l--;
+        else if(x-nums[l] > nums[h]-x) h++;
+        else l--;
+        
+    }
+    vector<int> ans;
+    for(int j = l+1; j<h; j++){
+        ans.push_back(nums[j]);
+    }   
+    
+    printlg(ans);
+    return ans;
+} 
+
 int main() {
     // vector<int> nums{10,22,25,35,40,49,57,58,68};
     vector<int> nums{1,1,2,3,4,5};
     int k = 4;
     int x = -1;
-    vector<int> kClosestNums = kClosest(nums, k, x);
-    printlg(kClosestNums);
+    // vector<int> kClosestNums = kClosest(nums, k, x);
+    // printlg(kClosestNums);
+
+    vector<int> kClosestNumsBS = kClosestBS(nums, k, x);
+    printlg(kClosestNumsBS);
     
     return 0;
 }
