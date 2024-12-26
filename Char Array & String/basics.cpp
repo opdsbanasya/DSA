6738d9a59008020775c341df541daed111b2e924
DHARM <opdsbanasya@gmail.com>
2024-12-26 21:50:15 +0530

    basic of char array

---
+++ Char Array & String/basics.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    // char name[100] = "Dharm Singh";
+    char name[100];
+    // cin>>name;
+    // cout<<name<<endl;
+
+    cout<<"enter "<<endl;
+    cin>>name[3];
+    cin>>name[50];
+
+    for(int i = 0; i<52; i++){
+        cout<<i <<" : "<<name[i]<<endl;
+
+    }
+
+    return 0;
+}
