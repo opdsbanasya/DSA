721c3960680e25632a815aa50e385871328e73fd
DHARM <opdsbanasya@gmail.com>
2025-01-22 23:34:04 +0530

    method 2

--- Char Array & String/removeAdjacentDuplicates.cpp
+++ Char Array & String/removeAdjacentDuplicates.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// without using new string
 string removeAdjacent(string str){
     int i = 0, j = i+1;
 
@@ -15,11 +16,30 @@ string removeAdjacent(string str){
     return str;
 }
 
+// using new string
+string removeAllAdjacent(string str){
+    string newstr = "";
+
+    int i = 0;
+
+    while(i<str.length()){
+        if(newstr.length() - 1 >= 0 && newstr[newstr.length()-1] == str[i]){
+            newstr.pop_back();
+        } else {
+            newstr.push_back(str[i]);
+        }
+        i++;
+    }
+    return newstr;
+}
+
 int main(){
     string str = "bbaacaab";
 
     cout<<str<<endl;
     string rmstr = removeAdjacent(str);
     cout<<rmstr<<endl;
+    string nrmstr = removeAllAdjacent(str);
+    cout<<nrmstr<<endl;
     return 0;
 }
