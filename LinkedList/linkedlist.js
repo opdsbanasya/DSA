561c02960a142bbb5654abd042a9da3b5168ffe6
DHARM <opdsbanasya@gmail.com>
2025-08-02 20:47:19 +0530

    Creating linkedlist and adding elements at tail and head

---
+++ LinkedList/linkedlist.js
@@ -0,0 +1,54 @@
+// Create a function that return an object with data and next
+function createNode(value) {
+  return {
+    data: value,
+    next: null,
+  };
+}
+
+// Add at Head
+function addAtHead(head, data) {
+  let newNode = createNode(data);
+  newNode.next = head;
+  head = newNode;
+  return newNode;
+}
+
+// Add at Tail
+function addAtTail(head, data){
+    // Edge Case -> if list empty
+    if(head == null){
+        return addAtHead(head, data);
+    }
+
+    let temp = head;
+    while(temp.next != null){
+        temp = temp.next;
+    }
+
+    let newNode = createNode(data);
+    temp.next = newNode;
+    return head;
+}
+
+// Display
+function displayList(head){
+    let temp = head;
+    let result = "";
+    while(temp != null){
+        result = result + temp.data + " → ";
+        temp = temp.next;
+    }
+    result = result + "X";
+    console.log(result);
+}
+
+let head = null;
+head = addAtHead(head, 10);
+head = addAtHead(head, 20);
+head = addAtHead(head, 30);
+head = addAtTail(head, 50);
+head = addAtTail(head, 40);
+displayList(head)
+
+// Add At Tail
