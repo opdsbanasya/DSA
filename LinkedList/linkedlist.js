54bb17646b9abf0958bf21d3b9df0d5194116e91
DHARM <opdsbanasya@gmail.com>
2025-08-02 23:33:15 +0530

    removeNode and addNode at ith index

--- LinkedList/linkedlist.js
+++ LinkedList/linkedlist.js
@@ -15,85 +15,87 @@ function addAtHead(head, data) {
 }
 
 // Add at Tail
-function addAtTail(head, data){
-    // Edge Case -> if list empty
-    if(head == null){
-        return addAtHead(head, data);
-    }
+function addAtTail(head, data) {
+  // Edge Case -> if list empty
+  if (head == null) {
+    return addAtHead(head, data);
+  }
 
-    let temp = head;
-    while(temp.next != null){
-        temp = temp.next;
-    }
+  let temp = head;
+  while (temp.next != null) {
+    temp = temp.next;
+  }
 
-    let newNode = createNode(data);
-    temp.next = newNode;
-    return head;
+  let newNode = createNode(data);
+  temp.next = newNode;
+  return head;
 }
 
 // Add At ith node
-function addAt(haed, i, data){
-    if(head == null) return;
-    let temp = head;
-    let count = 0;
-    while(count < i-1){
-        temp = temp.next;
-        count += 1;
-    }
+function addAt(haed, i, data) {
+  if (head == null) return;
+  let temp = head;
+  let count = 0;
+  while (count < i - 1) {
+    temp = temp.next;
+    count += 1;
+  }
 
-    let newNode = createNode(data);
-    let nextI = temp.next;
-    temp.next = newNode;
-    newNode.next = nextI;
+  let newNode = createNode(data);
+  let nextI = temp.next;
+  temp.next = newNode;
+  newNode.next = nextI;
 
-    return head;
+  return head;
 }
 
 // Remove From Head
-function removeAtHead(head){
-    if(head == null) return;
-    newHead = head.next;
-    head.next = null;
-    return newHead;
+function removeAtHead(head) {
+  if (head == null) return;
+  newHead = head.next;
+  head.next = null;
+  return newHead;
 }
 
 // Remove from Tail
-function removeAtTail(head){
-    if(head == null || head.next == null) return;
-    
-    let temp = head;
-    while(temp.next.next != null){
-        temp = temp.next;
-    }
-    temp.next = null;
-    return head;
+function removeAtTail(head) {
+  if (head == null || head.next == null) return;
+
+  let temp = head;
+  while (temp.next.next != null) {
+    temp = temp.next;
+  }
+  temp.next = null;
+  return head;
 }
 
-function removeAt(head, i){
-    if(head == null) return;
-    let temp = head;
-    let count = 0;
-    while(count < i-1){
-        temp = temp.next;
-        count += 1;
-    }
-    let nodeToBeRemoved = temp.next;
-    let nextOfRemovedNode = temp.next.next;
-    temp.next = nextOfRemovedNode;
-    nodeToBeRemoved.next = null;
-    return head;
+function removeAt(head, i) {
+  let temp = head;
+  if (head == null || temp.next == null) return;
+  if(i == 0) return removeAtHead(head);
+  let count = 0;
+  while (temp.next != null && count < i - 1) {
+    temp = temp.next;
+    count += 1;
+  }
+  if (temp.next == null && count <= i - 1) return head;
+  let nodeToBeRemoved = temp.next;
+  let nextOfRemovedNode = temp.next.next;
+  temp.next = nextOfRemovedNode;
+  nodeToBeRemoved.next = null;
+  return head;
 }
 
 // Display
-function displayList(head){
-    let temp = head;
-    let result = "";
-    while(temp != null){
-        result = result + temp.data + " → ";
-        temp = temp.next;
-    }
-    result = result + "X";
-    console.log(result);
+function displayList(head) {
+  let temp = head;
+  let result = "";
+  while (temp != null) {
+    result = result + temp.data + " → ";
+    temp = temp.next;
+  }
+  result = result + "X";
+  console.log(result);
 }
 
 let head = null;
@@ -109,7 +111,6 @@ head = addAtTail(head, 40);
 displayList(head);
 head = addAt(head, 2, 70);
 displayList(head);
-head = removeAt(head, 3);
-head = removeAt(head, 1);
+head = removeAt(head, 2);
 displayList(head);
 // Add At Tail
